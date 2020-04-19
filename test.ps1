d9ce8ac0890d0333f7772eea226c84c72e723d2f
Feodor Fitsner <feodor@appveyor.com>
2020-04-19 10:54:22 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -8,13 +8,13 @@ $images = (docker images)
 $images
 if ($images.length -ne ([int]$env:TOTAL_IMAGES + 1)) { throw "Wrong number of images!"; }
 
-$run1 = (docker run mcr.microsoft.com/windows/servercore:ltsc2016 cmd /c dir)
+$run1 = (docker run mcr.microsoft.com/windows/servercore:ltsc2019 cmd /c dir)
 $run1
-if ($run1[3].indexOf('Directory of C:\') -eq -1) { throw "Error running mcr.microsoft.com/windows/servercore:ltsc2016 container"; }
+if ($run1[3].indexOf('Directory of C:\') -eq -1) { throw "Error running mcr.microsoft.com/windows/servercore:ltsc2019 container"; }
 
-$run2 = (docker run mcr.microsoft.com/windows/nanoserver:sac2016 cmd /c dir)
+$run2 = (docker run mcr.microsoft.com/windows/nanoserver:1809 cmd /c dir)
 $run2
-if ($run2[3].indexOf('Directory of C:\') -eq -1) { throw "Error running mcr.microsoft.com/windows/nanoserver:sac2016 container"; }
+if ($run2[3].indexOf('Directory of C:\') -eq -1) { throw "Error running mcr.microsoft.com/windows/nanoserver:1809 container"; }
 
 $containers = (docker ps -a -q)
 $containers
