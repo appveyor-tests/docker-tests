98c1c9882a8dde0a42475049760e36e577ac5b85
Feodor Fitsner <feodor@appveyor.com>
2020-04-19 11:26:23 -0700

    Test LCOW and Linux modes

--- test.ps1
+++ test.ps1
@@ -19,3 +19,18 @@ if ($run2[3].indexOf('Directory of C:\') -eq -1) { throw "Error running mcr.micr
 $containers = (docker ps -a -q)
 $containers
 if ($containers.length -ne 2) { throw "Wrong number of containers!"; }
+
+# Testing LCOW mode if Experimental is set
+Write-Host "Testing LCOW..." -ForegroundColor Cyan
+$daemonConfig = Get-Content "$env:programdata\docker\config\daemon.json" | ConvertFrom-Json
+if ($daemonConfig.experimental) {
+  $results = (docker run --rm -v "$env:USERPROFILE`:/user-profile" busybox ls /user-profile) -join "`n"
+  if ($results.indexOf('Application Data') -eq -1) { throw "Error running busybox in LCOW mode"; }
+}
+
+# Testing Linux mode
+if (Get-Command Switch-DockerLinux -ErrorAction SilentlyContinue) {
+  Switch-DockerLinux
+  $results = (docker run --rm -v "C:\:/disk_c" busybox ls /disk_c) -join "`n"
+  if ($results.indexOf('Program Files') -eq -1) { throw "Error running busybox in Linux mode"; }  
+}
