e206a23ac5950eb553bdf08af0fe877da2825313
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2024-03-25 15:42:24 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -8,14 +8,11 @@ $images = (docker images --digests)
 $images
 if ($images.length -ne ([int]$env:TOTAL_IMAGES + 1)) { throw "Wrong number of images!"; }
 
-& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchWindowsEngine
-Start-Sleep -s 30
-
-$run1 = (docker run --isolation=hyperv mcr.microsoft.com/windows/servercore:ltsc2019 cmd /c dir)
+$run1 = (docker run mcr.microsoft.com/windows/servercore:ltsc2019 cmd /c dir)
 $run1
 if ($run1[3].indexOf('Directory of C:\') -eq -1) { throw "Error running mcr.microsoft.com/windows/servercore:ltsc2019 container"; }
 
-$run2 = (docker run --isolation=hyperv mcr.microsoft.com/windows/nanoserver:1809 cmd /c dir)
+$run2 = (docker run mcr.microsoft.com/windows/nanoserver:1809 cmd /c dir)
 $run2
 if ($run2[3].indexOf('Directory of C:\') -eq -1) { throw "Error running mcr.microsoft.com/windows/nanoserver:1809 container"; }
 
@@ -23,10 +20,6 @@ $containers = (docker ps -a -q)
 $containers
 if ($containers.length -ne 2) { throw "Wrong number of containers!"; }
 
-& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchLinuxEngine
-Start-Sleep -s 30
-$containers = (docker ps -a -q)
-$containers
 # Check "old" host records
 Get-Content "$env:windir\System32\drivers\etc\hosts"
 
